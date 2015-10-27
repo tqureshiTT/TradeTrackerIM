b966c41c0f020a81c3ce7d296e3c3d17fcf78e0a
Tahir Qureshi <tahir.tradetracker@gmail.com>
2015-10-27 01:55:30 -0400

    bug fix for composer

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,4 +2,6 @@
 
 cd /home/ec2-user/TradeTrackerIM
 chown -R ec2-user:nginx *
+chmod -R a+rwx /home/ec2-user/TradeTrackerIM/app/cache
 runuser -l ec2-user -c 'cd /home/ec2-user/TradeTrackerIM;/usr/local/bin/composer install'
+chmod -R a+rwx /home/ec2-user/TradeTrackerIM/app/cache
