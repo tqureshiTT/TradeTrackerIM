da5fb867cadb0bbcaa4c36354077ccd3c5860d1a
Tahir Qureshi <tahir.tradetracker@gmail.com>
2015-10-26 23:34:22 -0400

    bug fix for composer

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/ec2-user
-rm -Rf /home/ec2-user/TradeTrackerIM
-mkdir /home/ec2-user/TradeTrackerIM
-chown ec2-user:nginx /home/ec2-user/TradeTrackerIM
+#rm -Rf /home/ec2-user/TradeTrackerIM
+#mkdir /home/ec2-user/TradeTrackerIM
+#chown ec2-user:nginx /home/ec2-user/TradeTrackerIM
 
