ecc6ace0ef25c8865e175855ef750bf14bfcf8df
Tahir Qureshi <tahir.tradetracker@gmail.com>
2015-10-27 09:04:41 -0400

    added a use staement use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route

--- src/AppBundle/Controller/TradeTrackerIMController.php
+++ src/AppBundle/Controller/TradeTrackerIMController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Entity\People;
 use Symfony\Component\HttpFoundation\Response;
 use TradeTrackerIM\PeopleBundle\Document\dPeople;
