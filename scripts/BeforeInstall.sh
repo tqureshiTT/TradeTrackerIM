8bc3a34302a4f5bea2df2acf1caf00acdf2bb64f
Tahir Qureshi <tahir.tradetracker@gmail.com>
2015-10-26 22:09:39 -0400

    bug fix for beforeinstall.sh

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /home/ec2-user
-sudo rm -Rf /home/ec2-user/TradeTrackerIM
+rm -Rf /home/ec2-user/TradeTrackerIM
 mkdir /home/ec2-user/TradeTrackerIM
+chown ec2-user:nginx /home/ec2-user/TradeTrackerIM
 
