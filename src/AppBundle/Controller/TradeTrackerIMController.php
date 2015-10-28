6d8149fdcbc4b46b8c7a70dd54b510d1896cf8f1
Tahir Qureshi <tahir.tradetracker@gmail.com>
2015-10-28 10:34:51 -0400

    TearDown

--- src/AppBundle/Controller/TradeTrackerIMController.php
+++ src/AppBundle/Controller/TradeTrackerIMController.php
@@ -224,8 +224,8 @@ class TradeTrackerIMController extends Controller
     				//],
 			//]
 			//);
-		//$resultMessage=$result->search('Reservations.Instances[1]');	
-		$resultMessage=$result->search('Reservations')->valueOf();	
+		$resultMessage=$result->search('Reservations.Instances[1].InstanceId');
+		//$resultMessage=$result->search('Reservations')->valueOf();	
 		return new Response('<html><body>'.$resultMessage.'</body></html>');
 	}
 }
